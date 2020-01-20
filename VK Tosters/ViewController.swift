a999b2a0368f0fc1270ca49a5d90c9ee09c96048
programmist-NP662 <>
2020-01-20 15:16:52 +0500

    Upload Project

---
+++ VK Tosters/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Tosters
+//
+//  Created by programmist_np on 20/01/2020.
+//  Copyright © 2020 programmist_np. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
